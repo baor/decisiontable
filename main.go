42de9c09a22696b5aed2d07880ab910be0659b9f
Igor Rybak <irybak@travix.com>
2019-07-09 23:09:08 +0200

    cleanup code

--- main.go
+++ main.go
@@ -1,14 +1,3 @@
-/*
-* it is an exmaple of applying rules in go
-* pros:
-* 1. testing
-* 2. CI/CD development process
-* 3. best practicies from software development are applied
-* 4. shared work/ visibility with dev
-* Cons:
-* 1. Visibility with business
-* 2. table visual shift
- */
 package main
 
 import (
@@ -17,14 +6,14 @@ import (
 
 // domain is a structure of incoming request to which rules are applied
 type domain struct {
-	affiliate     string
-	channel       string
-	origin        string
-	destination   string
-	baggageMarkup float64
+	affiliate   string
+	channel     string
+	origin      string
+	destination string
+	markup      float64
 }
 
-func applyBaggageRules(r domain) domain {
+func applyRules(r domain) domain {
 	// condition is a flat structure which describes condition columns in decision table
 	type condition struct {
 		affiliate     interface{}
@@ -37,16 +26,15 @@ func applyBaggageRules(r domain) domain {
 
 	// action is a structure which describes a response in case of match in the table
 	type action struct {
-		isBaggageEnabled bool
-		baggageMarkup    float64
+		markup float64
 	}
 
 	// fill in decision table with rules
-	row(condition{affiliate: "CheapticketsNL", channel: ANY, origin: "AMS", destination: "LON"}, action{isBaggageEnabled: true, baggageMarkup: 2.0})
-	row(condition{affiliate: "BudgetAirNL", channel: ANY, origin: "AMS", destination: "LON"}, action{isBaggageEnabled: false})
-	row(condition{affiliate: ANY, channel: ANY, origin: "AMS", destination: "PAR"}, action{isBaggageEnabled: true, baggageMarkup: 1.0})
-	row(condition{affiliate: ANY, channel: ANY, origin: "AMS", destination: "LON", price: 11.22}, action{isBaggageEnabled: true, baggageMarkup: 1.0})
-	row(condition{affiliate: ANY, channel: ANY, origin: "AMS", destination: ne("LON"), price: 11.22}, action{isBaggageEnabled: true, baggageMarkup: 1.0})
+	row(condition{affiliate: "Aff1", channel: ANY, origin: "AMS", destination: "LON"}, action{markup: 2.0})
+	row(condition{affiliate: "Aff2", channel: ANY, origin: "AMS", destination: "LON"}, action{})
+	row(condition{affiliate: ANY, channel: ANY, origin: "AMS", destination: "PAR"}, action{markup: 1.0})
+	row(condition{affiliate: ANY, channel: ANY, origin: "AMS", destination: "LON", price: 11.22}, action{markup: 1.0})
+	row(condition{affiliate: ANY, channel: ANY, origin: "AMS", destination: ne("LON"), price: 11.22}, action{markup: 1.0})
 
 	// apply decision table to the request
 	res := apply(r)
@@ -62,28 +50,28 @@ func applyBaggageRules(r domain) domain {
 	fmt.Printf("action: %+v \n", act)
 
 	// assemble action data to response
-	r.baggageMarkup = act.baggageMarkup
+	r.markup = act.markup
 	return r
 }
 
 func main() {
 	r1 := domain{
-		affiliate:   "Vayama",
+		affiliate:   "Aff1",
 		origin:      "AMS",
 		destination: "LON",
 	}
 	fmt.Println("r1")
 	fmt.Printf("Before rule: %+v \n", r1)
-	r1 = applyBaggageRules(r1)
+	r1 = applyRules(r1)
 	fmt.Printf("After rule: %+v \n", r1)
 
 	r2 := domain{
-		affiliate:   "Vayama",
+		affiliate:   "Aff1",
 		origin:      "NONE",
 		destination: "LON",
 	}
 	fmt.Println("r2")
 	fmt.Printf("Before rule: %+v \n", r2)
-	r2 = applyBaggageRules(r2)
+	r2 = applyRules(r2)
 	fmt.Printf("After rule: %+v \n", r2)
 }
